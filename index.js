71fcb4e4f35e95de8d99b8e01421bba9b210ffb1
Juliette Knightley <knightleyjb@gmail.com>
2019-02-15 13:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"];
   console.log(kittens.slice(2));
+
+function appendKitten(name){
+  var newArray = kittens.slice();
+  newArray.push(name)
+  return newArray
+}
