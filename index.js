6e778fe6d0e30fe4932dbbd1baa1b34f47578d7b
Juliette Knightley <knightleyjb@gmail.com>
2019-02-15 13:38:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var kittens array1 = ["Milo", "Otis", "Garfield"];
-var kittens array2 = ["Elmo", "Skeeter"];
+var kittens array1 = ["Milo", "Otis", "Garfield"]
+var kittens array2 = ["Elmo", "Skeeter"]
 
 console.log(array1.concat(array2));
